5b40db4d32f36e3937f0170ce092cdab676d6a73
BobbertBurnsley <134723372+BobbertBurnsley@users.noreply.github.com>
2023-05-30 10:53:49 -0500

    Update peeaychpee.php

--- peeaychpee.php
+++ peeaychpee.php
@@ -6,33 +6,29 @@ $course = $_POST['course'];
 $professor = $_POST['professor'];
 $comments = $_POST['comments'];
 
-// Database connection
-$db = new SQLite3('data.db');
+// Connect to the main database
+$db = new SQLite3('main.db');
 
-// Insert form data into the main table
-$query = "INSERT INTO submissions (name, email, course, professor) VALUES ('$name', '$email', '$course', '$professor')";
-$db->exec($query);
+// Create the main table if it doesn't exist
+$db->exec('CREATE TABLE IF NOT EXISTS form_data (name TEXT, email TEXT, course TEXT, professor TEXT)');
+
+// Insert data into the main table
+$db->exec("INSERT INTO form_data (name, email, course, professor) VALUES ('$name', '$email', '$course', '$professor')");
+
+// Connect to the comments database
+$commentsDB = new SQLite3('comments.db');
+
+// Create the comments table if it doesn't exist
+$commentsDB->exec('CREATE TABLE IF NOT EXISTS comments (comment TEXT)');
 
 // Insert comments into the comments table
-$query = "INSERT INTO comments (comments) VALUES ('$comments')";
-$db->exec($query);
+$commentsDB->exec("INSERT INTO comments (comment) VALUES ('$comments')");
 
-// Generate the CSV file
-$csvData = [
-    ['Name', 'Email', 'Course', 'Professor'],
-    [$name, $email, $course, $professor]
-];
-$csvFilePath = 'submission.csv';
-
-$csvFile = fopen($csvFilePath, 'w');
-foreach ($csvData as $csvRow) {
-    fputcsv($csvFile, $csvRow);
-}
-fclose($csvFile);
-
-// Close the database connection
+// Close database connections
 $db->close();
+$commentsDB->close();
 
-// Send a response message
-echo 'Form Complete, Thank You!';
+// Send a success response
+echo "Form data and comments have been saved.";
 ?>
+
