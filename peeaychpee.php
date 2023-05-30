89e2ebc7adbac77ea48fa77659c4f1bb62bf8230
BobbertBurnsley <134723372+BobbertBurnsley@users.noreply.github.com>
2023-05-30 10:30:52 -0500

    Update peeaychpee.php

--- peeaychpee.php
+++ peeaychpee.php
@@ -1,14 +1,38 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+// Retrieve form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$course = $_POST['course'];
+$professor = $_POST['professor'];
+$comments = $_POST['comments'];
 
-    // Perform SQLite/C++ integration and generate CSV file
-    // Save the CSV file to the desired location
+// Database connection
+$db = new SQLite3('data.db');
 
-    // Return a response indicating success or failure
-    $response = "Form data has been saved and CSV file has been generated.";
-    echo $response;
+// Insert form data into the main table
+$query = "INSERT INTO submissions (name, email, course, professor) VALUES ('$name', '$email', '$course', '$professor')";
+$db->exec($query);
+
+// Insert comments into the comments table
+$query = "INSERT INTO comments (comments) VALUES ('$comments')";
+$db->exec($query);
+
+// Generate the CSV file
+$csvData = [
+    ['Name', 'Email', 'Course', 'Professor'],
+    [$name, $email, $course, $professor]
+];
+$csvFilePath = 'submission.csv';
+
+$csvFile = fopen($csvFilePath, 'w');
+foreach ($csvData as $csvRow) {
+    fputcsv($csvFile, $csvRow);
 }
+fclose($csvFile);
+
+// Close the database connection
+$db->close();
+
+// Send a response message
+echo 'Form Complete, Thank You!';
 ?>
