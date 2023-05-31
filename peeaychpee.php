9023f3320710cdff6a2f87e10e643410add1859b
BobbertBurnsley <134723372+BobbertBurnsley@users.noreply.github.com>
2023-05-31 13:28:16 -0500

    Update peeaychpee.php

--- peeaychpee.php
+++ peeaychpee.php
@@ -1,28 +1,56 @@
 <?php
 // Retrieve form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$course = $_POST['course'];
-$professor = $_POST['professor'];
-$comments = $_POST['comments'];
+$name = $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
+$course = $_POST['course'] ?? '';
+$professor = $_POST['professor'] ?? '';
+$comments = $_POST['comments'] ?? '';
 
 // Connect to the main database
 $db = new SQLite3('main.db');
 
+if (!$db) {
+    die("Connection to main database failed.");
+}
+
 // Create the main table if it doesn't exist
-$db->exec('CREATE TABLE IF NOT EXISTS form_data (name TEXT, email TEXT, course TEXT, professor TEXT)');
+$query = 'CREATE TABLE IF NOT EXISTS form_data (name TEXT, email TEXT, course TEXT, professor TEXT)';
+$result = $db->exec($query);
+
+if (!$result) {
+    die("Error creating main table: " . $db->lastErrorMsg());
+}
 
 // Insert data into the main table
-$db->exec("INSERT INTO form_data (name, email, course, professor) VALUES ('$name', '$email', '$course', '$professor')");
+$query = "INSERT INTO form_data (name, email, course, professor) VALUES ('$name', '$email', '$course', '$professor')";
+$result = $db->exec($query);
+
+if (!$result) {
+    die("Error inserting data into main table: " . $db->lastErrorMsg());
+}
 
 // Connect to the comments database
 $commentsDB = new SQLite3('comments.db');
 
+if (!$commentsDB) {
+    die("Connection to comments database failed.");
+}
+
 // Create the comments table if it doesn't exist
-$commentsDB->exec('CREATE TABLE IF NOT EXISTS comments (comment TEXT)');
+$query = 'CREATE TABLE IF NOT EXISTS comments (comment TEXT)';
+$result = $commentsDB->exec($query);
+
+if (!$result) {
+    die("Error creating comments table: " . $commentsDB->lastErrorMsg());
+}
 
 // Insert comments into the comments table
-$commentsDB->exec("INSERT INTO comments (comment) VALUES ('$comments')");
+$query = "INSERT INTO comments (comment) VALUES ('$comments')";
+$result = $commentsDB->exec($query);
+
+if (!$result) {
+    die("Error inserting comments into comments table: " . $commentsDB->lastErrorMsg());
+}
 
 // Close database connections
 $db->close();
